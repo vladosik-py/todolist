884d6771a7edaca05db850733edfc0f7c5dfe82f
vladosik_py <vbsmulskaya@gmail.com>
2023-07-18 14:08:13 +0300

    hw36

--- app/goals/serializers.py
+++ app/goals/serializers.py
@@ -60,7 +60,7 @@ class GoalCommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = GoalComment
         fields = '__all__'
-        read_only_fields = ("id", "created", "updated", "user", "goal")
+        read_only_fields = ("created", "updated", "user", "goal")
 
     def validate_goal(self, value: Goal) -> Goal:
         if value.status == Goal.Status.archived:
