392115733941ff04833226c2b6b84146b7b895aa
vladosik_py <vbsmulskaya@gmail.com>
2023-07-18 21:52:51 +0300

    hw37

--- app/goals/models.py
+++ app/goals/models.py
@@ -15,6 +15,9 @@ class Board(BaseModel):
     title = models.CharField(verbose_name="Название", max_length=255)
     is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
 
+    def __str__(self):
+        return self.title
+
 
 class BoardParticipant(BaseModel):
     class Meta:
