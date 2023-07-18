330f0e4c62eebd8b2228171b7358c6c463e7f012
vladosik_py <vbsmulskaya@gmail.com>
2023-07-18 20:12:32 +0300

    hw37

--- app/goals/urls.py
+++ app/goals/urls.py
@@ -3,19 +3,22 @@ from django.urls import path
 from goals import views
 
 urlpatterns = [
-
+    # Boards
     path("board/create", views.BoardCreateView.as_view(), name="create_board"),
     path("board/list", views.BoardListView.as_view(), name="board_list"),
     path("board/<int:pk>", views.BoardDetailView.as_view(), name="board_detail"),
 
+    # Categories
     path("goal_category/create", views.GoalCategoryCreateView.as_view(), name="create_category"),
     path("goal_category/list", views.GoalCategoryListView.as_view(), name="category_list"),
     path("goal_category/<int:pk>", views.GoalCategoryDetailView.as_view(), name="category_detail"),
 
+    # Goals
     path("goal/create", views.GoalCreateView.as_view(), name="create_goal"),
     path("goal/list", views.GoalListView.as_view(), name="goal_list"),
     path("goal/<int:pk>", views.GoalDetailView.as_view(), name="goal_detail"),
 
+    # Comments
     path("goal_comment/create", views.GoalCommentCreateView.as_view(), name="create_comment"),
     path("goal_comment/list", views.GoalCommentListView.as_view(), name="comment_list"),
     path("goal_comment/<int:pk>", views.GoalCommentDetailView.as_view(), name="comment_detail"),
