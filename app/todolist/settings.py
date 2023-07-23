b8acc51175d1bd67d48f452004746b667c45d844
vladosik_py <vbsmulskaya@gmail.com>
2023-07-23 18:47:29 +0300

    hw38

--- app/todolist/settings.py
+++ app/todolist/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         "NAME": env("DB_NAME"),
         "USER": env("DB_USER"),
         "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST", default='127.0.0.1'),
+        "HOST": 'db',
         "PORT": env("DB_PORT", default='5432'),
     }
 }
