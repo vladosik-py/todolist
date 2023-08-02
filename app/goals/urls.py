3a7654d49c3d9bd1cc0920df550f86cffa1a11bb
vladosik_py <vbsmulskaya@gmail.com>
2023-08-02 09:18:19 +0300

    tests

--- app/goals/urls.py
+++ app/goals/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from goals import views
 
+app_name = 'goals'
+
 urlpatterns = [
     # Boards
     path("board/create", views.BoardCreateView.as_view(), name="create_board"),
